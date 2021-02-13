f896dd3a426a073c1cadac88bd6e274f4949dac1
Torok Adam <adam-torok@outlook.hu>
2021-02-13 22:38:23 +0100

    Adding Vuex, Store, Routes init etc
    Initializing base project.

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+   <h1>{{welcome}}</h1>
+</template>
+
+<script>
+export default {
+   computed: {
+      welcome() {
+         return this.$store.getters.welcome;
+      },
+   },
+};
+</script>
